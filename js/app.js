a1859ab54a299a91fb72b5e20a7333fcffaf2990
derikhill <derikhill05@gmail.com>
2014-11-03 12:21:17 -0600

    updated JS

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
+$(document).ready(function() {	
+
+$('#enter-items').keydown(function(event){
+		if(event.keyCode == 13) {
+			event.preventDefault();
+			$('.add').click();
+		};
+	});
+
 	function addListItem() {
 		var text = $('#enter-items').val();
-		$('.item-list').append(+ text +);
+		$('#item-list').append('<p class="new-item">' + text + '</p>');
+		$('#enter-items').val('');
+		$('#item-list')	
+		.css('opacity', "0")
+        .css("margin-top", "-20px")
+        .animate(
+            { opacity: "1" },
+            { queue: true, duration: 'slow' }
+        )
+        .animate(
+            {marginTop: "0px"},
+            { queue: false, duration: 'slow' }
+        );
 	}
 
 	$(function() {
-		$('#add').on('click', addListItem);
-	});  
-
+		$('.add').on('click', addListItem);
+	}); 	
+});
